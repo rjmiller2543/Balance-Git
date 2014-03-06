af288d673c9e42d2197cf65db733e483660acf51
a7exarias <arias.alex.g@gmail.com>
2014-03-05 19:12:04 -0500

    test
    up

--- BalanceCopy2/CalendarCell.m
+++ BalanceCopy2/CalendarCell.m
@@ -15,6 +15,8 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
+        // Test comment
+        UIImage *image;
     }
     return self;
 }
